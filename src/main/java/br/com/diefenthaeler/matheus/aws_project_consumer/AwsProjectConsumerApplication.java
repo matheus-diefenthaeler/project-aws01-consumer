1c38a6205ee766d50d1d2f640cd94a6c9040f341
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-10-25 21:35:05 -0300

    Adicionando projeto

---
+++ src/main/java/br/com/diefenthaeler/matheus/aws_project_consumer/AwsProjectConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.diefenthaeler.matheus.aws_project_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjectConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProjectConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diefenthaeler/matheus/aws_project_consumer/AwsProjectConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diefenthaeler.matheus.aws_project_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjectConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
