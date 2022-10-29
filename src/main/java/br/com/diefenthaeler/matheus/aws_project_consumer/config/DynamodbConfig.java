c0af2ed35f2edc7b74e4b51f349a4fa78b90e851
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-10-29 17:10:00 -0300

    Configurando a aplicacao para acessar a tabela no DynamoDB

---
+++ src/main/java/br/com/diefenthaeler/matheus/aws_project_consumer/config/DynamodbConfig.java
@@ -0,0 +1,42 @@
+package br.com.diefenthaeler.matheus.aws_project_consumer.config;
+
+import br.com.diefenthaeler.matheus.aws_project_consumer.repository.ProductEventLogRepository;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@EnableDynamoDBRepositories(basePackageClasses = ProductEventLogRepository.class)
+public class DynamodbConfig {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    @Primary
+    public DynamoDBMapperConfig dynamoDBMapperConfig() {
+        return DynamoDBMapperConfig.DEFAULT;
+    }
+
+    @Bean
+    @Primary
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB, DynamoDBMapperConfig config) {
+        return new DynamoDBMapper(amazonDynamoDB, config);
+    }
+
+    @Bean
+    @Primary
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .withRegion(Regions.fromName(awsRegion)).build();
+    }
+}
