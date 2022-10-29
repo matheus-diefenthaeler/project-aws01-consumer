bfad5f58f09572672d972720f276cddf7c0f1fcc
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-10-29 17:11:48 -0300

    Persistindo os eventos de produtos

--- src/main/java/br/com/diefenthaeler/matheus/aws_project_consumer/service/ProductEventConsumer.java
+++ src/main/java/br/com/diefenthaeler/matheus/aws_project_consumer/service/ProductEventConsumer.java
@@ -1,8 +1,7 @@
 package br.com.diefenthaeler.matheus.aws_project_consumer.service;
 
-import br.com.diefenthaeler.matheus.aws_project_consumer.model.Envelope;
-import br.com.diefenthaeler.matheus.aws_project_consumer.model.ProductEvent;
-import br.com.diefenthaeler.matheus.aws_project_consumer.model.SnsMessage;
+import br.com.diefenthaeler.matheus.aws_project_consumer.model.*;
+import br.com.diefenthaeler.matheus.aws_project_consumer.repository.ProductEventLogRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +11,8 @@ import org.springframework.stereotype.Service;
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 
 @Service
 @Slf4j
@@ -19,6 +20,7 @@ import java.io.IOException;
 public class ProductEventConsumer {
 
     private final ObjectMapper objectMapper;
+    private final ProductEventLogRepository productEventLogRepository;
 
     @JmsListener(destination = "${aws.sqs.queue.product.events.name}")
     public void receivedProductEvent(TextMessage textMessage) throws JMSException, IOException {
@@ -34,5 +36,26 @@ public class ProductEventConsumer {
                 envelope.getEventType(),
                 productEvent.getProductId(),
                 snsMessage.getMessageId());
+
+        ProductEventLog productEventLog = buildProductEventLog(envelope, productEvent);
+        productEventLogRepository.save(productEventLog);
+
+    }
+
+    private ProductEventLog buildProductEventLog(Envelope envelope, ProductEvent productEvent){
+        long timestamp = Instant.now().toEpochMilli();
+
+        ProductEventLog productEventLog = new ProductEventLog();
+
+        productEventLog.setPk(productEvent.getCode());
+        productEventLog.setSk(envelope.getEventType() + "_" + timestamp);
+        productEventLog.setEventType(envelope.getEventType());
+        productEventLog.setProductId(productEvent.getProductId());
+        productEventLog.setUsername(productEvent.getUsername());
+        productEventLog.setTimestamp(timestamp);
+        productEventLog.setTtl(Instant.now().plus(Duration.ofMinutes(10)).toEpochMilli());
+
+        return productEventLog;
+
     }
 }
