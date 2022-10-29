e60fef726cfcc4bd14032508203fc71aac0ea782
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-10-29 17:11:00 -0300

    Criando modelo de evento do produto

---
+++ src/main/java/br/com/diefenthaeler/matheus/aws_project_consumer/model/ProductEventKey.java
@@ -0,0 +1,32 @@
+package br.com.diefenthaeler.matheus.aws_project_consumer.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import lombok.Getter;
+import lombok.Setter;
+
+public class ProductEventKey {
+    private String pk;
+    private String sk;
+
+    public ProductEventKey() {
+    }
+
+    @DynamoDBHashKey(attributeName = "pk")
+    public String getPk() {
+        return pk;
+    }
+
+    public void setPk(String pk) {
+        this.pk = pk;
+    }
+
+    @DynamoDBRangeKey(attributeName = "sk")
+    public String getSk() {
+        return sk;
+    }
+
+    public void setSk(String sk) {
+        this.sk = sk;
+    }
+}
